4df18a9a7a02a5293f061765ea64cd1dfd474990
dondix15 <dondix03@gmail.com>
2025-05-01 14:56:34 -0600

    progess commit

--- analyzer_app/views.py
+++ analyzer_app/views.py
@@ -2,6 +2,8 @@
 import os
 from django.http import HttpResponse
 from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 from django.shortcuts import render
 from .forms import UploadFileForm
 #views.py handles the file upload
@@ -12,7 +14,10 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         file = request.FILES['file']
-        return HttpResponse(f"name of uploaded file is {str(file)}")
+        fs = FileSystemStorage(location=settings.MEDIA_ROOT, base_url=settings.MEDIA_URL)
+        filename = fs.save( file.name, file)
+        file_url = fs.url(filename)
+        return render(request, 'analyzer_app/showimage.html', {'file_url': file_url})
     else:
         form = UploadFileForm()
         #return render(request, 'upload_success.html', {'file_path': full_path})
--- imageAnalyzer/settings.py
+++ imageAnalyzer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,10 +27,10 @@ SECRET_KEY = 'django-insecure-1gw=mrqdqnp9_)l9pzlv5#xwyxy^0(o%*6d$y#kj&g6n+vwtt+
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'analyzer_app'
 ]
 
 MIDDLEWARE = [
