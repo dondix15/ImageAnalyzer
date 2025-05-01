368a5cb8157ae8689b01aa24b65c8132ca4173a1
dondix15 <dondix03@gmail.com>
2025-05-01 11:54:42 -0600

    progress commit

--- analyzer_app/views.py
+++ analyzer_app/views.py
@@ -1,4 +1,20 @@
-from django.http import HttpResponse
 
+import os
+from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from django.shortcuts import render
+from .forms import UploadFileForm
+#views.py handles the file upload
 def home(request):
     return HttpResponse("Welcome to ImageAnalyzer!")
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        file = request.FILES['file']
+        return HttpResponse(f"name of uploaded file is {str(file)}")
+    else:
+        form = UploadFileForm()
+        #return render(request, 'upload_success.html', {'file_path': full_path})
+    return render(request, 'analyzer_app/upload.html', {'form': form})
+
